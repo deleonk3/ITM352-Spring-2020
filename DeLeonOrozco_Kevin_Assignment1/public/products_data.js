486ffa7585ec692e0a2c1cfb4028ecfeab64c4d2
deleonk3 <deleonk3@hawaii.edu>
2020-04-26 15:46:42 -1000

    Assignment1

---
+++ DeLeonOrozco_Kevin_Assignment1/public/products_data.js
@@ -0,0 +1,43 @@
+//Code used from Lab 13
+products=
+[
+  {  
+  model:"Spiderman",  
+  price: 40.00,  
+  image: "https://i.ebayimg.com/images/i/391963475481-0-1/s-l1000.jpg"
+  },
+
+{  
+  model:"SSGSS Goku",  
+  price: 45.00,  
+  image: "https://cdn.shopify.com/s/files/1/1782/0745/products/500pxl1_dragon-ball-super-goku-kamehameha-funko-pop-vinyl-figure-popcultcha-oob.1561344610.1570584283s_large.jpg?v=1572421846"
+  },
+
+{  
+    model:"SSGSS Vegeta",  
+    price: 73.00,  
+    image: "https://cconnect.s3.amazonaws.com/wp-content/uploads/2019/12/Funko-Pop-Dragon-Ball-Z-Figures-713-Vegeta-Powering-Up-Metallic-Chase-Chalice-Collectibles-Exclusive.jpg"
+    },
+
+{  
+    model:"Darth Vader",  
+    price: 10.00,  
+    image: "http://i.ebayimg.com/images/i/131178072220-0-1/s-l1000.jpg"
+    },
+
+{  
+    model:"Ironman",  
+    price: 30.00,  
+    image: "http://marveltoynews.com/wp-content/uploads/2015/01/Funko-Iron-Man-Age-of-Ultron-POP-Vinyls-Figure.jpg"
+    },
+
+{  
+     model:"Darth Maul",  
+     price: 1804.00,  
+     image: "https://s3.thcdn.com/productimg/960/960/11357697-8244433739810840.jpg"
+     }
+    ];
+
+    if (typeof module != 'undefined') {
+      module.exports.products = products;
+    }
